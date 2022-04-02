9b4dd1e30d413b0c4b50908f250370e2bc59e7fe
Maja Cvetkovic <maja.cvetkovic@degordian.com>
2022-04-02 17:32:37 +0200

    add image gallery component

--- src/components/ProductGallery/ProductGallery.js
+++ src/components/ProductGallery/ProductGallery.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState, useRef} from "react";
+
 import ImageGallery from 'react-image-gallery';
+import {ZoomBtnWrapper} from './style'
+
 import PackageIcon from '../../icons/package.svg'
-import {ReactComponent as Pack} from '../../icons/package.svg'
+import {ReactComponent as ZoomIn} from '../../icons/zoom-in.svg'
+import {ReactComponent as ZoomOut} from '../../icons/zoom-out.svg'
 
 
 const ProductGallery = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  let galleryRef = useRef();
+
+  const handleFullScreen = e => {
+    galleryRef.current.toggleFullScreen();
+    setIsFullscreen((prev) => !prev);
+  }
+
+  const renderCustomControls = () => {
+    return <ZoomBtnWrapper onClick={handleFullScreen}>
+            {isFullscreen ? <ZoomOut/> : <ZoomIn />}
+          </ZoomBtnWrapper>
+  }
+
   const images = [
     {
       original: PackageIcon,
@@ -13,15 +31,12 @@ const ProductGallery = () => {
     {
       original: PackageIcon,
       thumbnail: PackageIcon,
-    },
-    {
-      original: PackageIcon,
-      thumbnail: PackageIcon,
-    },
+    }
   ];
+
   return (
     <div>
-      <ImageGallery items={images} showFullscreenButton={false} showPlayButton={false} showNav={false} thumbnailPosition={'left'}/>;
+      <ImageGallery ref={galleryRef} items={images} renderFullscreenButton={renderCustomControls} showPlayButton={false} showNav={false} thumbnailPosition={'left'}/>;
     </div>
   )
 };
---
+++ src/components/ProductGallery/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import {IconButton} from "@mui/material";
+
+export const ZoomBtnWrapper = styled(IconButton)`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+    
+  svg {
+    width: 30px;
+    height: 30px;
+    fill: #A7A7A7;
+  }
+`
